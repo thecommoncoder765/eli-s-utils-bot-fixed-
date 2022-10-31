15ffa4a28c3dda2ecfed609597565f47b9399575
thecommoncoder7 <65053816+thecommoncoder765@users.noreply.github.com>
2022-10-31 20:58:41 +0000

    Add bot interactionCreate function

--- index.js
+++ index.js
@@ -22,5 +22,19 @@ client.slashcommands = new Discord.Collection()
 client.loadslashcommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
 client.loadslashcommands(bot, false)
 
+client.on("interactionCreate", (interaction) => {
+  if (!interaction.isCommand()) return
+  if (!interaction.inGuild()) return interaction.reply("This command can only be used in a server")
+
+  const slashcmd = client.slashcommands.get(interaction.commandName)
+
+  if (!slashcmd) return interaction.reply("This is an invalid slash command")
+
+  if (slashcmd.perm && !interaction.member.permissions.has(slashcmd.perm))
+    return interaction.reply("You do not meet the required permissions for this command")
+
+  slashcmd.run(client, interaction)
+})
+
 const token = process.env['TOKEN']
 client.login(token)
