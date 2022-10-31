82cec783a9038e4cd4890bc54509f4b463cd303d
thecommoncoder7 <65053816+thecommoncoder765@users.noreply.github.com>
2022-10-31 18:30:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js")
+
+const client = new Discord.Client({
+  intents: [
+    "Guilds",
+    "GuildMessages",
+    "MessageContent",
+    // GuildMembers
+  ]
+})
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`)
+})
