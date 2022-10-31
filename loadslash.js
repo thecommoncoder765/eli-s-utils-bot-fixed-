767fcf6f2a236ae072643600bd0ef671e8cc4c2d
thecommoncoder7 <65053816+thecommoncoder765@users.noreply.github.com>
2022-10-31 19:58:06 +0000

    Move client on ready after loadslashcommands

--- loadslash.js
+++ loadslash.js
@@ -13,14 +13,18 @@ let bot = {
   client
 }
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`)
-})
+const guildId = "1034240157815156776"
 
 client.slashcommands = new Discord.Collection()
 
 client.loadslashcommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
 client.loadslashcommands(bot, false)
+
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`)
+})
+
 
 const token = process.env['TOKEN']
 client.login(token)
